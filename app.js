d73278af46e9353bf8dbd5fe4fe8b819b8ee7677
Tim Kovacic <kovacictim3@gmail.com>
2023-10-05 22:44:43 -0400

    version 1.2

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.get('/prayer', prayerRouter);
+app.use('/prayer', prayerRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
