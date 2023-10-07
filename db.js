242189aa85e94db615e4d450c6f16519c5d1ee0c
Tim Kovacic <kovacictim3@gmail.com>
2023-10-07 16:07:01 -0400

    Update db.js

--- db.js
+++ db.js
@@ -2,43 +2,29 @@ const { Client } = require('pg');
 const settings = require("./config/localSettings.json");
 
 async function queryAllActivePrayerRequests() {
-    let {pgsql_host} = process.env.pgsql_host;
-    if (pgsql_host) {
-        console.log(pgsql_host.toString('utf8'));
-        try {
-            pgsql_host = JSON.parse(pgsql_host.toString('utf8'));
-        } catch (err) {
-            throw Error(
-                `Unable to parse secret from Secret Manager. Make sure that the secret is JSON formatted: ${err}`
-            );
-        }
-    } else {
+    let dbConfig = {
+        user: 'postgres',
+        password: '',
+        database: 'postgres',
+        host: '',
+        port: 5432,
+        ssl: false
+    };
+
+    let pgsql_host = process.env.pgsql_host;
+    if (null == pgsql_host) {
         const settings = require('./config/localSettings.json');
         pgsql_host = settings.pgsql_host;
     }
 
-    let {pgsql_pass} = process.env.pgsql_pass;
-    if (pgsql_pass) {
-        try {
-            pgsql_pass = JSON.parse(pgsql_pass.toString('utf8'));
-        } catch (err) {
-            throw Error(
-                `Unable to parse secret from Secret Manager. Make sure that the secret is JSON formatted: ${err}`
-            );
-        }
-    } else {
+    let pgsql_pass = process.env.pgsql_pass;
+    if (null == pgsql_pass) {
         const settings = require('./config/localSettings.json');
         pgsql_pass = settings.pgsql_pass;
     }
 
-    const dbConfig = {
-        user: 'postgres',
-        password: `${pgsql_pass}`,
-        database: 'postgres',
-        host: `${pgsql_host}`,
-        port: 5432,
-        ssl: false
-    };
+    dbConfig.host = `${pgsql_host}`;
+    dbConfig.host = `${pgsql_pass}`;
 
     console.log(dbConfig);
     const dbClient = new Client(dbConfig);
